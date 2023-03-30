e3b0a98264368f93abac99be7b634242dc877a73
Schäpertöns Veronika <veronika.schaepertoens@plus.ac.at>
2023-03-30 16:01:45 +0200

    initial commit

---
+++ scripts/01_merge_data.R
@@ -0,0 +1,76 @@
+## ---------------------------
+##
+## Script name: 01_merge_data
+##
+## Purpose of script: Loads and merges all datasets, loads meta data, saves merged dataset and meta data as matrices
+##
+## Author: Dr. Veronika Schäpertöns
+##
+## Date Created: 2023-03-31
+##
+## Copyright (c) Veronika Schäpertöns, 2023
+## Email: veronika.schaepertoens@plus.ac.at
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
+# load libraries ----------------------------------------------------------
+
+library(tidyverse)
+
+
+# import datasets ---------------------------------------------------------
+
+data_exp1 <- read_csv("data/Experiment1_Protein_Groups_log2_Normalized_substract_Median_only_valid_values.csv",
+                      col_select = c(1:9,30))
+
+data_exp2 <- read_csv("data/Experiment2_Protein_Groups_log2_Normalized_substract_Median_only_valid_values.csv",
+                      col_select = c(1:9,30))
+
+data_exp3 <- read_csv("data/Experiment3_new_Protein_Groups_log2_Normalized_substract_Median_only_valid_values.csv",
+                      col_select = c(1:9,30))
+
+data_meta <- read_csv("data/pData.csv")
+
+# merge datasets by protein ids -------------------------------------------
+
+data.matrix <- data_exp3 %>% 
+  left_join(data_exp1, by = "protein_id") %>% 
+  left_join(data_exp2, by = "protein_id") %>%
+  na.omit() %>%
+  tibble::column_to_rownames("protein_id") %>%
+  as.matrix()
+
+dim(data.matrix)
+
+colnames(data.matrix) <- data_meta$sample #rename columns of data.matrix
+design.matrix <- data_meta %>% as.matrix()
+
+# save data ---------------------------------------------------------------
+
+save(data.matrix, design.matrix, file="analysis/01_merge_data/all_data.RData")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
