67ed926501ef2739f05567f92ad88c670fce674d
VSchaepertoens <vschaepertoens@protonmail.com>
2023-11-14 10:17:12 +0100

    merges pheatmap plotting functions

--- scripts/05_plot_dge_results.R
+++ scripts/05_plot_dge_results.R
@@ -25,11 +25,12 @@ library(pheatmap)
 
 # load data ---------------------------------------------------------------
 
-res <- readRDS("analysis/04_map_protein_to_gene/results_dge_gene.rds")
+res <- readRDS("analysis/results_dge.rds")
 
-data.matrix.batch <- readRDS("analysis/02_explore_data/batch_corrected_all_data.rds")
+#data.matrix.batch <- readRDS("analysis/02_explore_data/batch_corrected_all_data.rds")
+load(file = "analysis/all_data.RData")
 
-load(file = "analysis/04_map_protein_to_gene/matched_treated_batch_corrected_all_data.RData")
+#load(file = "analysis/04_map_protein_to_gene/matched_treated_batch_corrected_all_data.RData")
 
 
 # plot results ------------------------------------------------------------
@@ -64,88 +65,136 @@ ggplot(pDT[adj.P.Val < 0.05], aes(x = logFC_hpyl, y = logFC_drfae)) +
 
 
 # plot heatmaps of log2 values --------------------------------------------
-
-# function to plot all proteins for a certain condition
-plot_genes <- function(mat,
-                       comparison,
-                       detailed_annotation = FALSE,
-                       top_n = Inf) {
-  selected_genes <- 
-    res[coef == "drfae_vs_hpyl"] %>%
-    as_tibble() %>% 
-    mutate(abs_logFC = abs(logFC)) %>% 
-    slice_max(n = top_n, abs_logFC) %>% 
-    pull(Gene)
-  
-  if (detailed_annotation) {
-    annx <- dcast.data.table(res, rn ~ coef, value.var = "logFC")
-    ann.row <- data.frame(row.names = annx$rn, annx[, -"rn", with = FALSE])
-  } else {
-    ann.row <- with(
-      res[coef == comparison],
-      data.frame(row.names = rn, logFC = ifelse(logFC > 0, "up", "down"))
-    )
-  }
-  
-  pheatmap(mat[res[coef == comparison][adj.P.Val < 0.05]$rn,],
-           annotation_colors = list(logFC = c(down = "blue",up = "red")),
-           annotation_row = ann.row,
-           cluster_cols = TRUE,
-           show_rownames = FALSE,
-           scale = "row")  
-}
-
-# heatmap of drfae vs untreated, all significant genes, all samples
-svg("figures/heatmap_drfae_vs_untreated.svg")
-plot_genes(data.matrix.batch, "drfae")
-
-dev.off()
-
-# heatmap of hpyl vs untreated, all significant genes, all samples
-svg("figures/heatmap_hpyl_vs_untreated.svg")
-plot_genes(data.matrix.batch, "hpyl")
-
-dev.off()
-
-# heatmap of drfae vs hpyl, all significant genes, all samples
-plot_genes(data.matrix.batch, "drfae_vs_hpyl")
-
-# heatmap of drfae vs hpyl, all significant genes, all samples, ANNOTATE by actual logFC values
-plot_genes(data.matrix.batch, "drfae_vs_hpyl", TRUE)
+# 
+# # function to plot all proteins for a certain condition
+# plot_genes <- function(mat,
+#                        comparison,
+#                        detailed_annotation = FALSE,
+#                        top_n = Inf) {
+#   # selected_genes <- 
+#   #   res[coef == "drfae_vs_hpyl"] %>%
+#   #   as_tibble() %>% 
+#   #   mutate(abs_logFC = abs(logFC)) %>% 
+#   #   slice_max(n = top_n, abs_logFC) %>% 
+#   #   pull(Gene)
+#   
+#   if (detailed_annotation) {
+#     annx <- dcast.data.table(res, rn ~ coef, value.var = "logFC")
+#     ann.row <- data.frame(row.names = annx$rn, annx[, -"rn", with = FALSE])
+#   } else {
+#     ann.row <- with(
+#       res[coef == comparison],
+#       data.frame(row.names = rn, logFC = ifelse(logFC > 0, "up", "down"))
+#     )
+#   }
+#   
+#   ann_df <- data.frame(rn = res[coef == comparison][adj.P.Val < 0.05]$rn)
+#   
+#   pheatmap(mat[ann_df$rn,],
+#            annotation_colors = list(logFC = c(down = "blue",up = "red")),
+#            annotation_row = ann.row,
+#            cluster_cols = TRUE,
+#            show_rownames = FALSE,
+#            scale = "row")  
+# }
+# 
+# # heatmap of drfae vs untreated, all significant genes, all samples
+# svg("figures/heatmap_drfae_vs_untreated.svg")
+# plot_genes(data.matrix.batch, "drfae")
+# 
+# dev.off()
+# 
+# # heatmap of hpyl vs untreated, all significant genes, all samples
+# svg("figures/heatmap_hpyl_vs_untreated.svg")
+# plot_genes(data.matrix.batch, "hpyl")
+# 
+# dev.off()
+# 
+# # heatmap of drfae vs hpyl, all significant genes, all samples
+# plot_genes(data.matrix.batch, "drfae_vs_hpyl")
+# 
+# # heatmap of drfae vs hpyl, all significant genes, all samples, ANNOTATE by actual logFC values
+# plot_genes(data.matrix.batch, "drfae_vs_hpyl", TRUE)
 
 
 
 ## function to plot top genes
 plot_top_genes <- function(mat, 
+                           comparison,
+                           top_genes = FALSE,
                            no_genes = 20,
                            show_rownames = FALSE){
   
-  ann_df <- data.frame(logFC = res[coef == "drfae_vs_hpyl"][adj.P.Val < 0.05]$logFC,
-                       rn = res[coef == "drfae_vs_hpyl"][adj.P.Val < 0.05]$rn,
-                       gene = res[coef == "drfae_vs_hpyl"][adj.P.Val < 0.05]$Gene)
+  ann_df <- data.frame(logFC = res[coef == comparison][adj.P.Val < 0.05]$logFC,
+                       rn = res[coef == comparison][adj.P.Val < 0.05]$rn,
+                       gene = res[coef == comparison][adj.P.Val < 0.05]$Gene)
   
-  ann_df <- ann_df[order(abs(ann_df$logFC), decreasing = TRUE),]  
+  if (top_genes) {
+    ann_df <- ann_df[order(abs(ann_df$logFC), decreasing = TRUE),]  
+    ann.row <- with(ann_df[1:no_genes,], 
+                  data.frame(row.names = gene,log2FC = ifelse(logFC > 0, "up","down")))
   
-  ann.row <- with(ann_df[1:no_genes,], data.frame(row.names = gene,log2FC = ifelse(logFC > 0, "up","down")))
-  
-  pheatmap(mat[ann_df$gene[1:no_genes], ], 
-           annotation_colors = list(log2FC = c(down = "blue",up = "red")),
-           annotation_row = ann.row,
-           cluster_cols = T,
-           show_rownames = show_rownames,
-           scale = "row")
-  }
+    pheatmap(mat[ann_df$gene[1:no_genes], ], 
+             annotation_colors = list(log2FC = c(down = "blue",up = "red")),
+             annotation_row = ann.row,
+             cluster_cols = T,
+             show_rownames = show_rownames,
+             scale = "row")
+  } else {
+    ann.row <- with(ann_df,
+                    data.frame(row.names = gene, logFC = ifelse(logFC > 0, "up", "down")))
+    pheatmap(mat[ann_df$gene,],
+             annotation_colors = list(logFC = c(down = "blue",up = "red")),
+             annotation_row = ann.row,
+             cluster_cols = TRUE,
+             show_rownames = FALSE,
+             scale = "row") 
+    }
+}
+
+# heatmap of drfae vs untreated, all significant genes, all samples
+plot_top_genes(data.matrix.batch.matched, "drfae")
+
+# heatmap of hpyl vs untreated, all significant genes, all samples
+plot_top_genes(data.matrix.batch.matched, "hpyl")
+
+# heatmap of drfae vs hpyl, all significant genes, all samples
+plot_top_genes(data.matrix.batch.matched, "drfae_vs_hpyl")
+
+
+# subset only treated batch-corrected samples -----------------------------
+
+data.matrix.batch.matched.treated <- data.matrix.batch.matched[, -grep("un", 
+                                                                       colnames(data.matrix.batch.matched))]
 
 # heatmap of drfae vs hpyl, top 20 significant genes, treated samples
-plot_top_genes(data.matrix.batch.treated.matched, 20, TRUE)
+plot_top_genes(mat = data.matrix.batch.matched.treated, 
+               comparison = "drfae_vs_hpyl",
+               top_genes = TRUE, 
+               no_genes = 20, 
+               show_rownames = TRUE)
+
 # heatmap of drfae vs hpyl, top 40 significant genes, treated samples
-plot_top_genes(data.matrix.batch.treated.matched, 40, TRUE)
+plot_top_genes(data.matrix.batch.matched.treated, 
+               "drfae_vs_hpyl", 
+               TRUE,
+               40, 
+               TRUE)
+
 # heatmap of drfae vs hpyl, top 60 significant genes, treated samples
-plot_top_genes(data.matrix.batch.treated.matched, 60, TRUE)
+plot_top_genes(data.matrix.batch.matched.treated,
+               "drfae_vs_hpyl", 
+               TRUE,
+               60, 
+               TRUE)
 
 # heatmap of drfae vs hpyl, all significant genes, treated samples
-svg("figures/heatmap_dfrae_vs_wt.svg")
-plot_top_genes(data.matrix.batch.treated.matched, 427, FALSE) 
+svg("figures/heatmap_drfae_vs_wt.svg")
+plot_top_genes(data.matrix.batch.matched.treated, 
+               "drfae_vs_hpyl", 
+               TRUE,
+               427, 
+               FALSE) 
 
 dev.off()
 
